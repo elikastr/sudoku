57a852466c829e81f2b04078e82e940cc4b78d95
Elika Ron <elikastr@gmail.com>
2022-07-22 22:50:45 +0300

    code cleanup

--- solver.py
+++ solver.py
@@ -47,28 +47,44 @@ class Solver(object):
             
             return False
         
-        return backtrack()
+        if not backtrack(): print("No solution\n")
+        else: self.print_board()
     
     def print_board(self):
         s = ""
 
-        if self.solve():
-            r_count = 0
-            for r in range(9):
-                r_count += 1
-                c_count = 0
-                for c in range(9):
-                    s += str(self.board[r][c]) + ' '
-                    c_count += 1
+        r_count = 0
+        for r in range(9):
+            r_count += 1
+            c_count = 0
+            for c in range(9):
+                s += str(self.board[r][c]) + ' '
+                c_count += 1
 
-                    if c_count % 3 == 0:
-                        if c_count == 9:
-                            s += '\n'
-                        else:
-                            s += '| '
-                if r_count % 3 == 0 and r_count != 9:
-                    s += '- - - - - - - - - - -\n'
-        else:
-            s = "No solution\n"
+                if c_count % 3 == 0:
+                    if c_count == 9:
+                        s += '\n'
+                    else:
+                        s += '| '
+            if r_count % 3 == 0 and r_count != 9:
+                s += '- - - - - - - - - - -\n'
         
         print(s)
+
+# def main():
+#     board = [[5,3,0,0,7,0,0,0,0]
+#             ,[6,0,0,1,9,5,0,0,0]
+#             ,[0,9,8,0,0,0,0,6,0]
+#             ,[8,0,0,0,6,0,0,0,3]
+#             ,[4,0,0,8,0,3,0,0,1]
+#             ,[7,0,0,0,2,0,0,0,6]
+#             ,[0,6,0,0,0,0,2,8,0]
+#             ,[0,0,0,4,1,9,0,0,5]
+#             ,[0,0,0,0,8,0,0,7,9]]
+    
+#     solver = Solver(board)
+#     solver.print_board()
+#     solver.solve()
+
+# if __name__ == '__main__':
+#     main()
