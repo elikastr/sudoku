428f49bd714af7fc45d89f2460fcb8e75dfdb2cc
Elika Ron <elikastr@gmail.com>
2022-07-23 20:15:19 +0300

    cleanup

--- solver.py
+++ solver.py
@@ -90,25 +90,3 @@ class Solver(object):
                 s += '- - - - - - - - - - -\n'
         
         print(s)
-
-
-# def main():
-#     board = [[5,3,0,0,7,0,0,0,0]
-#             ,[6,0,0,1,9,5,0,0,0]
-#             ,[0,9,8,0,0,0,0,6,0]
-#             ,[8,0,0,0,6,0,0,0,3]
-#             ,[4,0,0,8,0,3,0,0,1]
-#             ,[7,0,0,0,2,0,0,0,6]
-#             ,[0,6,0,0,0,0,2,8,0]
-#             ,[0,0,0,4,1,9,0,0,5]
-#             ,[0,0,0,0,8,0,0,7,9]]
-    
-#     solver = Solver(board)
-#     # solver.print_board()
-#     solver.solve()
-#     solver.print_board()
-#     # print(solver.solutions)
-
-
-# if __name__ == '__main__':
-#     main()
