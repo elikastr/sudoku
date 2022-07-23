62fdec5a92e5751b84461651eff2a422b5255724
Elika Ron <elikastr@gmail.com>
2022-07-23 15:59:15 +0300

    finished Generator class

--- generator.py
+++ generator.py
@@ -1,13 +1,26 @@
 from solver import Solver
+from random import choice
 
 class Generator(Solver):
     def __init__(self):
         board = [[0] * 9 for _ in range(9)]
         Solver.__init__(self, board)
 
-        self.fill()
-        self.__filled = [(r, c) for c in range(9) for r in range(9)]     # indices of filled cells
+        self.backtrack(random=True)    # randomly fill the board using the backtracking algorithm form Solver
+        self.__filled = [(r, c) for c in range(9) for r in range(9)]    # indices of filled cells
+        self.__remove()
 
+    
+    # remove random cells to generate a puzzle
+    def __remove(self, n=5):
+        while n > 0:
+            r, c = choice(self.__filled)
+            temp = self.board[r][c]
+            self.board[r][c] = 0
+            self.__filled.remove((r, c))
 
-    def fill(self):
-        self.backtrack(random=True)
+            copy = list(self.board)
+            if Solver(copy).num_solutions() != 1:
+                self.board[r][c] = temp
+                self.__filled.append((r, c))
+                n -= 1
