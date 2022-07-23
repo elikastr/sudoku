c704cea20b95b12ccf30d9a6f3fcf2b34301580e
Elika Ron <elikastr@gmail.com>
2022-07-23 15:15:40 +0300

    added fill() method

--- generator.py
+++ generator.py
@@ -1,7 +1,15 @@
 import collections
+from random import randint, shuffle
 from solver import Solver
 
 class Generator(Solver):
     def __init__(self):
-        board = [[0] * 9] * 9
+        board = [[0] * 9 for _ in range(9)]
         Solver.__init__(self, board)
+
+        self.fill()
+        self.__filled = [(r, c) for c in range(9) for r in range(9)]     # indices of filled cells
+
+
+    def fill(self):
+        self.backtrack(random=True)
