8776e66edcd9d98b643906958f9c1f8a1c57a4fe
Elika Ron <elikastr@gmail.com>
2022-07-25 18:34:58 +0300

    cleanup generator.py

--- generator.py
+++ generator.py
@@ -8,21 +8,21 @@ class Generator(Solver):
         Solver.__init__(self, board)
 
         self.backtrack(random=True)    # randomly fill the board using the backtracking algorithm form Solver
-        self.__filled = [(r, c) for c in range(9) for r in range(9)]    # indices of filled cells
+        self.filled = [(r, c) for c in range(9) for r in range(9)]    # indices of filled cells
 
         self.solved_board = deepcopy(self.board)
-        self.__remove()
+        self.remove()
 
     
     # remove random cells to generate a puzzle
-    def __remove(self, n=5):
+    def remove(self, n=5):
         while n > 0:
-            r, c = choice(self.__filled)
+            r, c = choice(self.filled)
             temp = self.board[r][c]
             self.board[r][c] = 0
-            self.__filled.remove((r, c))
+            self.filled.remove((r, c))
 
             if Solver(self.board).num_solutions() != 1:
                 self.board[r][c] = temp
-                self.__filled.append((r, c))
+                self.filled.append((r, c))
                 n -= 1
