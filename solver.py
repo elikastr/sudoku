f09a35d9c6b9f86927c5f6656399c79d977bbd65
Elika Ron <elikastr@gmail.com>
2022-07-25 18:27:10 +0300

    cleanup solver.py

--- solver.py
+++ solver.py
@@ -5,21 +5,28 @@ class Solver(object):
     def __init__(self, board):
         self.board = board
         self.solutions = 0
-        self.__rows = collections.defaultdict(set)     # rows[i] == list of filled digits in row i
-        self.__cols = collections.defaultdict(set)     # cols[i] == list of all filled digits in col i
-        self.__squares = collections.defaultdict(set)  # square[r // 3, c // 3] == list of all filled digits in the square
-        self.__empty = []                              # indices of empty cells
+        self.rows = collections.defaultdict(set)     # rows[i] == list of filled digits in row i
+        self.cols = collections.defaultdict(set)     # cols[i] == list of all filled digits in col i
+        self.squares = collections.defaultdict(set)  # square[r // 3, c // 3] == list of all filled digits in the square
+        self.empty = []                              # indices of empty cells
 
         for r in range(9):
             for c in range(9):
                 num = self.board[r][c]
 
                 if num == 0:
-                    self.__empty.append((r, c))
+                    self.empty.append((r, c))
                 else:
-                    self.__rows[r].add(num)
-                    self.__cols[c].add(num)
-                    self.__squares[r // 3, c // 3].add(num)
+                    self.rows[r].add(num)
+                    self.cols[c].add(num)
+                    self.squares[r // 3, c // 3].add(num)
+
+    
+    # check if number n is valid in position [r, c]
+    def valid(self, r, c, n):
+        return not (n in self.rows[r] or
+                    n in self.cols[c] or
+                    n in self.squares[r // 3, c // 3])
 
 
     # find solution using backtracking algorithm
@@ -28,41 +35,42 @@ class Solver(object):
     #                    board reverts to starting state
     # if random == True, try to fill with numbers in a random order
     def backtrack(self, solve=True, random=False):
-        if len(self.__empty) == 0: 
+        if len(self.empty) == 0: 
             self.solutions += 1
             return True
 
-        r, c = self.__empty[-1]
+        r, c = self.empty[-1]
 
         nums = [1,2,3,4,5,6,7,8,9]
         if random: shuffle(nums)
         for n in nums:
-            if (n in self.__rows[r] or
-                n in self.__cols[c] or
-                n in self.__squares[r // 3, c // 3]): continue
+            if not self.valid(r, c, n): continue
             
             self.board[r][c] = n
-            self.__rows[r].add(n)
-            self.__cols[c].add(n)
-            self.__squares[r // 3, c // 3].add(n)
-            self.__empty.pop()
+            self.rows[r].add(n)
+            self.cols[c].add(n)
+            self.squares[r // 3, c // 3].add(n)
+            self.empty.pop()
 
             if self.backtrack(solve, random) and solve: return True
 
             self.board[r][c] = 0
-            self.__rows[r].remove(n)
-            self.__cols[c].remove(n)
-            self.__squares[r // 3, c // 3].remove(n)
-            self.__empty.append((r, c))
+            self.rows[r].remove(n)
+            self.cols[c].remove(n)
+            self.squares[r // 3, c // 3].remove(n)
+            self.empty.append((r, c))
         
         return False
 
 
+    # find 1 solution and fill board
     def solve(self):
         return self.backtrack()
 
 
+    # find number of all possible solutions without filling board
     def num_solutions(self):
+        self.solutions = 0
         self.backtrack(solve=False)
         return self.solutions
 
